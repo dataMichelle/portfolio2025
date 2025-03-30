d7ade9fb7f9aeff88c4dac04cf6e7e25f001feb4
Michelle <116389047+dataMichelle@users.noreply.github.com>
2025-03-29 22:44:00 -0500

    fixed merge conflictin App

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-=======
-import ScrollToTop from "./components/ScrollToTop"; // Import the ScrollToTop component
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
->>>>>>> dev
 import Projects from "./components/Projects";
 import ProjectDetail from "./components/ProjectDetail";
 
 function App() {
   return (
     <Router>
-      <ScrollToTop /> {/* Add ScrollToTop here */}
       <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
         <Navbar />
-<<<<<<< HEAD
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:projectURL" element={<ProjectDetail />} />
-        </Routes>
-=======
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/project/:projectURL" element={<ProjectDetail />} />
           </Routes>
         </main>
->>>>>>> dev
       </div>
     </Router>
   );
